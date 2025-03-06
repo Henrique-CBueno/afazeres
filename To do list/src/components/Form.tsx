90e057c0c388c27c42dafae8fb5ca8bfd8143035
Henrique-CBueno <henriquecb2507@outlook.pt>
2025-03-06 10:41:36 -0300

    refactor Form component to manage input state and sanitize user input

--- To do list/src/components/Form.tsx
+++ To do list/src/components/Form.tsx
@@ -1,17 +1,30 @@
-import { CiCirclePlus } from 'react-icons/ci';
+import { useState } from "react";
+import { CiCirclePlus } from "react-icons/ci";
 
 export function Form() {
+  const [text, setText] = useState("");
 
-    function handleCreateTask() {
-        
-    }
-    
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const onlyLetters = e.target.value.replace(/[^a-zA-ZÀ-ÿ\s]/g, "").trim();
+    setText(onlyLetters);
+    console.log(onlyLetters)
+  };
 
-    return (
-        <form className="flex justify-center items-center gap-3.5 -mt-8">
-            <input placeholder='Adicione uma nova tarefa' type="text" className="bg-gray-500 w-2xl border-none outline-none p-5 rounded-md text-base placeholder-gray-300"/>
-            <button className='flex items-center justify-center gap-2 bg-gray-400 w-24 h-16 cursor-pointer rounded-md text-sm transition-all duration-500 text-white border-none outline-none hover:bg-amber-300
-            '>Criar <span><CiCirclePlus color='white'/></span></button>
-        </form>
-    )
+  return (
+    <form className="flex justify-center items-center gap-3.5 -mt-8">
+      <input
+        placeholder="Adicione uma nova tarefa"
+        type="text"
+        className="bg-gray-500 w-2xl border-none outline-none p-5 rounded-md text-base placeholder-gray-300"
+        value={text}
+        onChange={handleChange}
+      />
+      <button
+        className="flex items-center justify-center gap-2 bg-gray-400 w-24 h-16 cursor-pointer rounded-md text-sm transition-all duration-500 text-white border-none outline-none hover:bg-amber-300"
+      >
+        Criar <span><CiCirclePlus color="white" /></span>
+      </button>
+    </form>
+  );
 }
+
